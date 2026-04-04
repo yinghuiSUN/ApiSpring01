117b10f86c46fb0aaaea8b446474c14cd0a1871f
sunyinghui <sunyinghui0506@gmail.com>
2026-04-04 11:36:41 +0200

    YSUN - commit test

---
+++ src/main/java/net/javaguides/springboot_search_rest_api/entity/Account.java
@@ -0,0 +1,24 @@
+package net.javaguides.springboot_search_rest_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "account_holder_name")
+    private String accountHolderName;
+    private Double balance;
+
+}
