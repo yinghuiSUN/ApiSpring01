9578705bcc4f08a7a4c5acbbc302efec5f81a0e9
sunyinghui <sunyinghui0506@gmail.com>
2026-05-08 09:25:48 +0200

    YSUN: 2.01 commit

--- src/main/java/net/javaguides/springboot_search_rest_api/controller/ConnexionController.java
+++ src/main/java/net/javaguides/springboot_search_rest_api/controller/ConnexionController.java
@@ -1,5 +1,7 @@
 package net.javaguides.springboot_search_rest_api.controller;
 
+import net.javaguides.springboot_search_rest_api.dto.ApiResponse;
+import net.javaguides.springboot_search_rest_api.dto.ConnexionDto;
 import net.javaguides.springboot_search_rest_api.dto.UtilisateurDto;
 import net.javaguides.springboot_search_rest_api.service.UtilisateurService;
 import org.springframework.http.HttpStatus;
@@ -7,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/connexion")
+@RequestMapping("api")
 public class ConnexionController {
 
     private final UtilisateurService utilisateurService;
@@ -16,13 +18,14 @@ public class ConnexionController {
         this.utilisateurService = utilisateurService;
     }
 
-    @PostMapping("checkConnexion")
-    ResponseEntity<String> checkConnexion(@RequestParam(name ="name") String name, @RequestParam (name ="password") String password) {
+    @PostMapping("/checkConnexion")
+    ResponseEntity<ApiResponse<String>> checkConnexion(@RequestBody ConnexionDto connexionDto) {
         UtilisateurDto dto = new UtilisateurDto();
-        dto.setName(name);
-        dto.setPassword(password);
+        dto.setName(connexionDto.getName());
+        dto.setPassword(connexionDto.getPassword());
         String message = utilisateurService.checkConnexion(dto);
-        return new ResponseEntity<>(message, HttpStatus.OK);
+
+        return ResponseEntity.ok(new ApiResponse<>(message, message, HttpStatus.OK.value()));
 
     }
 }
